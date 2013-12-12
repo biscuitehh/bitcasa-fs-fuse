fd5865a60275931d54bc438398fb9087151dd99f
Michael Thomas <michael.thomas@biscuitlabs.com>
2013-12-12 22:31:46 +0000

    Last old style commit before new changes (as described in issues). Moving from hack to sexy implementation

--- bitcasa.py
+++ bitcasa.py
@@ -1,12 +1,55 @@
 # Bitcasa Python Class (Unofficial)
 # Michael Thomas, 2013 
+# TODO #
+########
+# Test for Python2 & Python3
+# Error Handling that works well.
+# Implement File Methods
+# DONE: download file to cache
+# upload file from cache
 
 # Import Section
 import urllib2, urllib
 import json
 import base64
 import sys
+import os
 
+# Upload Progress Helper
+# @todo - May switch Download to this as well.
+class Progress(object):
+	def __init__(self):
+		self._seen = 0.0
+
+	def update(self, total, size, name):
+		self._seen += size
+		pct = (self._seen / total) * 100.0
+		print '%s progress: %.2f' % (name, pct)
+
+class file_with_callback(file):
+	def __init__(self, path, mode, callback, *args):
+		file.__init__(self, path, mode)
+		self.seek(0, os.SEEK_END)
+		self._total = self.tell()
+		self.seek(0)
+		self._callback = callback
+		self._args = args
+
+	def __len__(self):
+		return self._total
+
+	def read(self, size):
+		data = file.read(self, size)
+		self._callback(self._total, len(data), *self._args)
+		return data
+# e.g.
+# path = 'large_file.txt'
+# progress = Progress()
+# stream = file_with_callback(path, 'rb', progress.update, path)
+# req = urllib2.Request(url, stream)
+# res = urllib2.urlopen(req)
+
+# Bitcasa Class
 class Bitcasa:
 	# Start Client & Load Config
 	def __init__ (self, config_path):
@@ -24,6 +67,17 @@ class Bitcasa:
 		self.redirect_url = self.config['redirect_url'].encode('utf-8')
 		self.auth_token = self.config['auth_token'].encode('utf-8')
 		self.access_token = self.config['access_token'].encode('utf-8')
+		
+		# Adding support for File Cache
+		self.cache_dir = self.config['cache_dir'].encode('utf-8')
+		# Check to see if cache_dir is valid & exists
+		if(self.cache_dir == None):
+			self.cache_dir = os.path.dirname(os.path.realpath(__file__)) + ".cache"
+			self.save_config()
+		# Now Make sure it exists
+		if not os.path.exists(self.cache_dir):
+			os.makedirs(self.cache_dir)
+		
 		# See if we need our tokens
 		if(self.auth_token == "") or (self.access_token == ""):
 			return self.authenticate()
@@ -32,7 +86,7 @@ class Bitcasa:
 
 	def save_config (self):
 		with open(self.config_path, 'w') as outfile:
-			json.dump(self.config, outfile)
+			json.dump(self.config, outfile, indent=4)
 
 	def authenticate (self):
 		print("### ENTER THE FOLLOWING URL IN A BROWSER AND AUTHORIZE THIS APPLICATION ###")
@@ -64,9 +118,8 @@ class Bitcasa:
 		else:
 			return response['result']['items']
 
-	def add_folder (self, folder_name, path = ""):
+	def add_folder (self, path, folder_name):
 		payload = {"folder_name":folder_name}
-		print(self.api_url + "/folders/" + path + "?access_token=" + self.access_token, urllib.urlencode(payload))
 		request = urllib2.Request(self.api_url + "/folders/" + path + "?access_token=" + self.access_token, urllib.urlencode(payload))
 		try:
 			response = json.load(urllib2.urlopen(request))
@@ -75,13 +128,36 @@ class Bitcasa:
 		return response
 
 	def delete_folder (self, path):
-		if(path != ""):
-			path = base64.b64encode(path)
-		request = urllib2.Request(self.api_url + "/folders/" + path + "?access_token=" + self.access_token, data)
+		payload = {"path":path}
+		request = urllib2.Request(self.api_url + "/folders/?access_token=" + self.access_token, urllib.urlencode(payload))
 		request.get_method = lambda: 'DELETE'
 		response = json.load(urllib2.urlopen(request))
 		return response
 
+	# File API Methods
+	def download_file (self, file_id, path, file_name, file_size):
+		f = open(self.cache_dir + "/" + file_name, 'wb')
+		print "Downloading file from: " + self.api_url + "/files/"+file_id+"/"+ file_name +"?access_token=" + self.access_token + "&path=/" + path
+		u = urllib2.urlopen(self.api_url + "/files/"+file_id+"/"+file_name+"?access_token=" + self.access_token + "&path/" + path)
+		print "Downloading: %s Bytes: %s" % (file_name, file_size)
+		file_size_dl = 0
+		block_sz = 8192
+		
+		while True:
+			buffer = u.read(block_sz)
+			if not buffer:
+				break
+			file_size_dl += len(buffer)
+			f.write(buffer)
+			status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+			status = status + chr(8)*(len(status)+1)
+			print status
+		print "Closing new File"
+		f.close()
+		return self.cache_dir + "/" + file_name
+
+	def upload_file (self, path, file_name, file_size):
+		return
 	# def rename_folder (self, path, new_filename, exists = "rename"):
 	# 	# Encode path
 	# 	if(path != ""):
--- bitcasafs.py
+++ bitcasafs.py
@@ -25,7 +25,7 @@ class BitcasaStat(fuse.Stat):
 		else:
 			self.st_mode = stat.S_IFREG | 0666
 			self.st_nlink = 1
-			self.st_size = item['size']
+			self.st_size = item['Size']
 		# TODO: Find something to set these with.
 		self.st_uid = os.geteuid()
 		self.st_gid = os.getgid()
@@ -43,6 +43,8 @@ class BitcasaFS(fuse.Fuse):
 		self.bitcasa = Bitcasa('config.json')
 		if(self.bitcasa == None):
 			sys.exit("Failed to authenticate Bitcasa Client.")
+		# Bitcasa Encoded Path (for things like rename/create/delete)
+		self.bpath = ""
 		# Breadcrumbs to how we got where we are.
 		self.breadcrumbs = {}
 		# Files/Folders in Current Path
@@ -56,10 +58,15 @@ class BitcasaFS(fuse.Fuse):
 			t[3] = 2; t[6] = 2048
 			return t
 		# Else pass File/Folder Object
-		else: return BitcasaStat(self.dir[path.split('/')[-1]])
+		# mkdir Check
+		elif(path.split('/')[-1] in self.dir):
+			return BitcasaStat(self.dir[path.split('/')[-1]])
+		else:
+			# DNE
+			return -errno.ENOENT
 
+	# Directory Methods
 	def readdir(self, path, offset):
-		print "readir: ",path
 		# Turn English into Bitcasa Base64
 		# Root Path - This clears our breadcrumbs @ preps
 		if(path == "/"):
@@ -69,26 +76,64 @@ class BitcasaFS(fuse.Fuse):
 			self.breadcrumbs = { }
 			# Add Root Breadcrumb
 			self.breadcrumbs['/'] = { 'Name':'', 'Path':'/', 'Type':'folders', 'mtime':'0'}
+			# Reset Path
+			self.bpath = ""
 		else:
 			# Load next round of Files/Folders from Bitcasa
 			bdir = self.bitcasa.list_folder(self.dir[path.split('/')[-1]]['Path'])
 			# Add our new Breadcrumb
 			# TODO - add logic to check and see if we have this breadcrumb already.
 			self.breadcrumbs[path.split('/')[-1]] = self.dir[path.split('/')[-1]]
+			# Current Bitcasa Path
+			self.bpath = self.dir[path.split('/')[-1]]['Path']
+			print(self.bpath)
 		for b in bdir:
-			item = { 'Name':b['name'].encode('utf-8'), 'Path':b['path'].encode('utf-8'), 'Type':b['category'], 'mtime':b['mtime'] }
-			# But wait, there's more!
-			if('size' in b):
-				item['size'] = b['size']
+			# Get Extra File Stuff
+			if(b['category'] == 'folders'):
+				item = { 'Name':b['name'].encode('utf-8'), 'Path':b['path'].encode('utf-8'), 'Type':b['category'], 'mtime':b['mtime'] }
+			else:
+				item = { 'Name':b['name'].encode('utf-8'), 'Path':b['path'].encode('utf-8'), 'Type':b['category'], 'mtime':b['mtime'], 'ID':b['id'].encode('utf-8'), 'Size':b['size'] }
 			self.dir[b['name'].encode('utf-8')] = item
 			# Now yield to FUSE
 			yield fuse.Direntry(b['name'].encode('utf-8'))
 
-	# WIP #
 	def mkdir(self, path, mode):
-		self.bitcasa.add_folder(self.current_item['Path'], "Test")
-		return 0
+		result = self.bitcasa.add_folder(self.bpath, path.split('/')[-1])
+		if(result['error'] == None):
+			new = result['result']['items'][0]
+			item = { 'Name':new['name'].encode('utf-8'), 'Path':new['path'].encode('utf-8'), 'Type':new['category'], 'mtime':new['mtime'] }
+			self.dir[new['name'].encode('utf-8')] = item
+			return 0
+		else:
+			return -errno.ENOSYS
 
+	# WIP - Doesn't report "file not found"
+	def rmdir(self, path):
+		print("Removing " + path + ";CurrentPath: " + self.bpath)
+		result = self.bitcasa.delete_folder(self.dir[path.split('/')[-1]]['Path'])
+		if(result['error'] == None):
+			return 0
+		else:
+			return -errno.ENOSYS
+
+	# File Methods
+	def open(self, path, flags):
+		print "Trying to open: ", path + "/" + self.dir[path.split('/')[-1]]['ID']
+		print "Filename is: ", self.dir[path.split('/')[-1]]['Name']
+		temp_file = self.bitcasa.download_file(self.dir[path.split('/')[-1]]['ID'], self.dir[path.split('/')[-1]]['Path'], self.dir[path.split('/')[-1]]['Name'], self.dir[path.split('/')[-1]]['Size'])
+		if temp_file != None:
+			return open(temp_file, "rb")
+		else:
+			return -errno.EACCESS
+
+	# Read from Cached File
+	# @todo - Consider adding "streaming" mode, we could do it.
+	def read(self, path, size, offset, fh):
+		fh.seek(offset)
+		return fh.read(size)
+
+
+# return -errno.ENOENT
 if __name__ == '__main__':
 	fs = BitcasaFS()
 	fs.parse(errex=1)
